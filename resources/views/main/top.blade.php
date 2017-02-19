66287d1f3594136e8f0b8cb976b6847a6db1e0a5
Unilorn <b5159@oic.jp>
2017-02-20 00:23:55 +0900

    Viewファイル生成

---
+++ resources/views/main/top.blade.php
@@ -0,0 +1,14 @@
+@extends('main.common.app')
+
+@section('head')
+    <script src="/js/main/top.js"></script>
+    <link rel="stylesheet" href="/css/main/top.css">
+@endsection
+
+@section('title',config('column.SiteName'))
+
+@section('content')
+
+
+
+@endsection
