7c1e78b663a6d65af3398def69b21924b17caf3d
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2017-06-20 02:50:25 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::post('/git/pulling',function (\Illuminate\Http\Request $request){
     $pass = $request->input('pass');
     if($pass == sha1(env('GIT_PASS'))){
         exec('/var/www/html/UnilornComHomePage/git pull origin master', $op, $rv);
-        return "Success!!!\n";
+        return "Success!!!\n".$op." : ".$rv;
     }
     return "Bad...\n ";
 });
