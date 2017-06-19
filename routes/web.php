a924e00026054981af113549eefea889b5fdecc9
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2017-06-20 01:40:27 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,10 @@ Route::get('/', function () {
 
 Route::post('/git/pulling',function (\Illuminate\Http\Request $request){
     $pass = $request->header('X-Hub-Signature');
-    if($pass == env('GIT_PASS')){
+    if($pass == "sha1=".sha1(env('GIT_PASS'))){
         exec('git pull origin master');
         return "Success!!!\n";
     }
-    var_dump($request->header('X-Hub-Signature'));
     return "Bad...\n ";
 });
 
