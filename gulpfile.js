4b8889ec8510357484cfdf8917b6475962c28105
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2017-06-23 17:37:15 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,10 @@ gulp.task('sass', function() {
     gulp.src(path.join(assetsPath, 'scss/main.scss'))
         .pipe(plumber())
         .pipe(sass())
-        .pipe(autoprefixer())
+        .pipe(autoprefixer({
+            browsers: ['last 2 version', 'iOS >= 8.1', 'Android >= 4.4'],
+            cascade: false
+        }))
         .pipe(gulp.dest(path.join(assetsPath,'./public/css')));
 });
 
