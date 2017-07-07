3131cdaa10f53a67267b45fe876860a2876603ff
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2017-07-08 00:27:55 +0900

    update

---
+++ app/Service/GithubLanguages.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Service;
+
+class GithubLanguages
+{
+
+    private $apt_url = "https://api.github.com/";
+    public function getLanguages(){
+        $list = [];
+        return $list;
+    }
+}
